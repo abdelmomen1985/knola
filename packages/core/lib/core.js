3b189543724f5d88a1fafa5ecf6f3ac453e6d08c
abdelmomen <abdelmomen1985@gmail.com>
2022-05-01 16:35:32 +0200

    0.0.5

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.logUser = void 0;
 function logUser(user) {
-    console.log(`${user.id} -> ${user.name} (${user.age ? user.age : 'age unknown'})` + `.`);
+    console.log(`# ${user.id} -> ${user.name} (${user.age ? user.age : 'age unknown'})`);
 }
 exports.logUser = logUser;
 //# sourceMappingURL=core.js.map
