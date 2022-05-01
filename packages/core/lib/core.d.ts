7372ddd16b9be89d793458d85074cf3ba6d9adcb
abdelmomen <abdelmomen1985@gmail.com>
2022-05-01 15:59:40 +0200

    built

---
+++ packages/core/lib/core.d.ts
@@ -0,0 +1,8 @@
+declare type User = {
+    id: string;
+    name: string;
+    age?: number;
+};
+export declare function logUser(user: User): void;
+export {};
+//# sourceMappingURL=core.d.ts.map
---
+++ packages/core/lib/core.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.logUser = void 0;
+function logUser(user) {
+    console.log(`${user.id}: ${user.name} (${user.age ? user.age : 'age unknown'})`);
+}
+exports.logUser = logUser;
+//# sourceMappingURL=core.js.map
